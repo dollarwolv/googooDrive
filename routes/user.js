ec7a0e3deae4b28d8a7745e93b05133211037c9d
dollarwolv <dollarwolv@gmail.com>
2025-10-25 21:51:59 +0800

    Add ability to add folders

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,11 @@ const prisma = require("../db/prisma");
 
 userRouter.get("/:id/folders", async (req, res) => {
   const id = parseInt(req.params.id);
+
+  if (id != res.locals.currentUser.id) {
+    return res.status(403).send("You don't have access to this page.");
+  }
+
   const user = await prisma.user.findUnique({
     where: {
       id,
@@ -20,6 +25,38 @@ userRouter.get("/:id/folders", async (req, res) => {
     },
   });
   res.render("user", { folders: folders, user: user });
+});
+
+userRouter.get("/:id/folders/new", async (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id != res.locals.currentUser.id) {
+    return res.status(403).send("You don't have access to this page.");
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+  res.render("new-folder", { user });
+});
+
+userRouter.post("/:id/folders/new", async (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id != res.locals.currentUser.id) {
+    return res.status(403).send("You can't do that.");
+  }
+  const { name } = req.body;
+  const folder = await prisma.folder.create({
+    data: {
+      userId: id,
+      name,
+    },
+  });
+  res.redirect(`/users/${id}/folders`);
 });
 
 module.exports = userRouter;
